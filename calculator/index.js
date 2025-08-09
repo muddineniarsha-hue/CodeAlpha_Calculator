8cf865938a5f519f37d5d65af7054691b302fa80
muddineniarsha-hue <muddineniarsha@gmail.com>
2025-08-09 10:21:25 +0530

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,28 @@
+let inputbox = document.getElementById('input-box');
+let buttons = document.querySelectorAll('button');
+
+let results = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            results = eval(results);
+            inputbox.value = results;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            results = "";
+            inputbox.value = results;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            results = results.substring(0, results.length-1);
+            inputbox.value = result;
+        }
+        else{
+            results += e.target.innerHTML;
+            inputbox.value = results;
+        }
+        
+    })
+})
